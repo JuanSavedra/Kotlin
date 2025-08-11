50cbb3588ca3f6aa8e2477c5f6bdd4faa0e4e571
JuanSavedra <juansavedra827@gmail.com>
2025-08-11 17:31:36 -0300

    comments and variables

---
+++ src/Comments.kt
@@ -0,0 +1,7 @@
+class Comments {
+    // Linha única
+
+    /*
+    Linha composta
+     */
+}
---
+++ src/Variables.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    //String
+    var courseName = "Análise e desenvolvimento de sistemas"
+    println(courseName)
+
+    courseName = "Ciência da Computação"
+    println(courseName)
+
+    //Inteiros
+    var age: Byte = 19 //Indicando que é um tipo Byte (só aceita números inteiros)
+    println(age)
+
+    age = 20
+    println(age)
+
+    //Variáveis constantes
+    val pi = 3.14
+    println(pi)
+
+    //Tipos numéricos
+    /*
+    Long - 64 bits (Inteiro)
+    Int - 32 bits (Inteiro)
+    Short - 16 bits (Inteiro)
+    Byte - 8 bits (Inteiro)
+    Double - 64 bits (Decimal)
+    Float - 32 bits (Decimal)
+     */
+
+    println(Long.MAX_VALUE)
+    println(Int.MAX_VALUE)
+    println(Short.MAX_VALUE)
+    println(Byte.MAX_VALUE)
+    println(Double.MAX_VALUE)
+    println(Float.MAX_VALUE)
+}
